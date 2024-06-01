20a57327959f50f10ca00baafc4882b5b082d60f
Mantresh <ray779@tuta.io>
2024-06-02 01:10:04 +0530

    working fp test impl

--- tests/fp.cpp
+++ tests/fp.cpp
@@ -2,34 +2,44 @@
 #include <format>
 #include <cstdint>
 #include <cmath>
-#include <sstream>
+#include <random>
 using namespace std;
 
-using base_t = uint64_t;
-using base_half_t = uint32_t;
+using base_double_t = uint64_t;
+using base_t = uint32_t;
+using base_half_t = uint16_t;
 
-std::string get_raw_repr(base_t n, bool hex = true)
+template<class F>
+std::string get_raw_repr(F n, bool hex = true)
 {
+	const auto bits = sizeof(F) * 8, bits_half = bits / 2;
 	if (hex)
-		return format("I:0x{:0>8x} F:0x{:0>8x}", n >> 32, n & ~base_half_t(0));
+		return format("I:0x{:0>8x} F:0x{:0>8x}",
+					  n >> bits_half, (n << bits_half) >> bits_half);
 	else
-		return format("I:{} F:{}", n >> 32, n & ~base_half_t(0));
+		return format("I:{} F:{}", n >> bits_half, (n << bits_half) >> bits_half);
 }
 
-std::string get_repr(base_t n)
+template<class F>
+std::string get_repr(F n)
 {
-	const bool neg = n >> 63;
+	const auto bits = sizeof(F) * 8, bits_half = bits / 2;
+	const bool neg = n >> (bits - 1);
 	if (neg)
 		n = ~n + 1;
 
-	const base_half_t integer = n >> 32, fraction = n & ~base_half_t(0);
+	const F integer = n >> bits_half, fraction = (n << bits_half) >> bits_half;
 
 	long double fraction_ld = 0;
 	base_t powers = 2;
-	for (int i = sizeof(base_half_t) * 8 - 1; i >= 0; i--)
+	for (int i = bits_half-1; i >= 0; i--)
 	{
 		if ((fraction >> i) & 0x1)
-			fraction_ld += 1.0L / powers;
+		{
+			const auto c = 1.0L / powers;
+			if (!isinf(c))
+				fraction_ld += c;
+		}
 		powers *= 2;
 	}
 
@@ -43,35 +53,61 @@ std::string get_repr(base_t n)
 		}
 	fstr = fstr.substr(i);
 
-	std::ostringstream oss;
-	if (neg) oss << '-';
-	oss << integer << "." << fstr;
-
-	return oss.str();
+	return format("{}{}.{}", neg ? "-" : "", integer, fstr);
 }
 
-base_t make_fp(base_half_t integer, base_half_t fraction, bool neg)
+template<class F>
+F make_fp(F integer, F fraction, bool neg)
 {
-	base_t n = (base_t(integer) << 32) | base_t(fraction);
+	const auto bits = sizeof(F) * 8, bits_half = bits / 2;
+	F n = (integer << bits_half) | fraction;
 	if (neg)
 		n = ~n + 1;
 	return n;
 }
 
-std::string get_both_repr(base_t n)
+template<class F>
+std::string get_both_repr(F n)
 {
 	return format("raw: {}, readable: {}", get_raw_repr(n), get_repr(n));
 }
 
+base_double_t mul(base_t aa, base_t bb)
+{
+	base_double_t product = 0;
+	const base_double_t a = aa >> 31 ? (~base_double_t(0) << 32 | aa) : aa;
+	const base_double_t b = bb >> 31 ? (~base_double_t(0) << 32 | bb) : bb;
+
+	size_t i = 0;
+	while (i < sizeof(base_double_t)*8)
+	{
+		auto bit = (b >> i) & 0x1;
+		auto shifted_a = a << i;
+
+		if (bit)
+			product += shifted_a;
+
+		i++;
+	}
+
+	return product;
+}
+
 int main()
 {
-	const auto n1 = make_fp(0, 0b11 << 30, false);
-	const auto n2 = make_fp(0, 0b01 << 30, false);
+	random_device rd;
+	mt19937 re(rd());
+	uniform_int_distribution<base_half_t> dist(0, ~base_half_t(0)), dist2(0, 20), dist3(0, 1);
+
+	const auto n1 = make_fp<base_t>(dist2(re), dist(re), dist3(re));
+	const auto n2 = make_fp<base_t>(dist2(re), dist(re), dist3(re));
 	const auto s = n1 + n2;
 	const auto d = n1 - n2;
+	const auto p = mul(n1, n2);
 
 	println("n1: {}", get_both_repr(n1));
 	println("n2: {}", get_both_repr(n2));
 	println("s: {}", get_both_repr(s));
 	println("d: {}", get_both_repr(d));
+	println("p: {}", get_both_repr(p));
 }
